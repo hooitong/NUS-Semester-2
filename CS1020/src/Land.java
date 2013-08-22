872e2ca0ec037c48c2b18522919a848d4dcd6f8f
Yeap Hooi Tong <hooitongx@gmail.com>
2013-08-22 22:12:24 +0800

    Land update

---
+++ CS1020/src/Land.java
@@ -0,0 +1,60 @@
+/**
+ * This class accepts 3 types of input and produce the appropriate output based on given operation.
+ * @author Yeap Hooi Tong
+ * @matric A0111736M
+ * @date 21/8/13
+ * @version 1.0
+ */
+
+import java.util.*;
+
+class Grid {
+
+    // declare the member field
+
+    // declare the constructor
+
+    /**
+     * checkNoTree   : to check whether the (size x size) square with upper-left coordinate
+     * (x, y) contains a tree
+     * Pre-condition :
+     * Post-condition:
+     */
+    public boolean checkNoTree(int x, int y, int size) {
+        // implementation
+    }
+
+    /**
+     * checkValidSize: to check whether it is possible to find a (size x size) square that contains
+     * no tree
+     * Pre-condition :
+     * Post-condition:
+     */
+    public boolean checkValidSize(int size) {
+    }
+
+
+    /**
+     * solve         : use this method to find the largest size of a square with no trees
+     * Pre-condition :
+     * Post-condition:
+     */
+    public int solve() {
+        // implementation
+    }
+
+}
+
+public class Land {
+
+    public static void main(String[] args) {
+
+        // declare the necessary variables
+
+        // create new object from class Result
+
+        // declare a Scanner object to read input
+
+        // read input and process them accordingly
+    }
+}
