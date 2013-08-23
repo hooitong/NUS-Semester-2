01cf61076fbb8c7f2654bdc8acda1b3fd4366911
Yeap Hooi Tong <hooitongx@gmail.com>
2013-08-23 20:11:40 +0800

    initial implementation of transformation

--- CS1020/src/Transformation.java
+++ CS1020/src/Transformation.java
@@ -49,7 +49,8 @@ public class Transformation {
     }
 
     /**
-     * This method reads the number
+     * This method reads the number of iterations that the user wants and accepts
+     * user's operation and appropriate sub-digit and process it and return to parent
      *
      * @param sc     The scanner object
      * @param matrix The matrix object
@@ -59,37 +60,92 @@ public class Transformation {
         // get the number of iterations that the user request for
         for (int iteration = sc.nextInt(); iteration > 0; iteration--) {
             String operation = sc.next();
-            if (operation.equals("Rotate"))
-                rotate(sc.nextInt());
-            else if (operation.equals("Reflect")) {
-                if (sc.next().equals("x")) reflectX();
-                else reflectY();
+
+            if (operation.equals("Rotate")) // if user decide to rotate
+                matrix = rotate(sc.nextInt(), matrix); // specify angle by user
+            else if (operation.equals("Reflect")) { // if user decides to reflect
+                if (sc.next().equals("x")) matrix = reflectX(matrix); // on x axis
+                else matrix = reflectY(matrix); // on y axis
             }
         }
 
+        // return the matrix back to parent
         return matrix;
     }
 
     /**
-     * This method reads the input and creates the 2D array based on user's size N
-     * It then populates the array with the given matrix provided by the user.
+     * This method prints out the matrix object into table form
      *
      * @param matrix The matrix object
      */
     private static void printMatrix(int[][] matrix) {
-
+        // read matrix given by user and print it out in table form
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                System.out.print(matrix[row][column] + " ");
+            }
+            System.out.print("\n");
+        }
     }
 
-    private static void rotate(int degree) {
+    /**
+     * This method is used to rotate the matrix by the specified degree by the user
+     *
+     * @param matrix The matrix object
+     */
+    private static int[][] rotate(int degree, int[][] matrix) {
+        // declare and initialise return matrix
+        int[][] processedMatrix = new int[matrix.length][matrix[0].length];
 
+        // based on the user's degree, determine the number of operation
+        for (int iteration = (degree / 90); iteration > 0; iteration--) {
+            for (int row = 0; row < matrix.length; row++) {
+                for (int column = 0; column < matrix[row].length; column++) {
+                    processedMatrix[row][column] = matrix[matrix.length - 1 - column][row];
+                }
+            }
+        }
+
+        // return new matrix
+        return processedMatrix;
     }
 
-    private static void reflectX() {
+    /**
+     * This method is used to reflect the matrix on the X axis
+     *
+     * @param matrix The matrix object
+     */
+    private static int[][] reflectX(int[][] matrix) {
+        // declare and initialise return matrix
+        int[][] processedMatrix = new int[matrix.length][matrix[0].length];
 
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                processedMatrix[row][column] = matrix[matrix.length - 1 - row][column];
+            }
+        }
+
+        // return new matrix
+        return processedMatrix;
     }
 
-    private static void reflectY() {
+    /**
+     * This method is used to reflect the matrix on the Y axis
+     *
+     * @param matrix The matrix object
+     */
+    private static int[][] reflectY(int[][] matrix) {
+        // declare and initialise return matrix
+        int[][] processedMatrix = new int[matrix.length][matrix[0].length];
 
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                processedMatrix[row][column] = matrix[row][matrix.length - 1 - column];
+            }
+        }
+
+        // return new matrix
+        return processedMatrix;
     }
 
 }
