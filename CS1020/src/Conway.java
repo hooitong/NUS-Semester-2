a10b6103445b92264819c78fc3deca6f67b59a88
Yeap Hooi Tong <hooitongx@gmail.com>
2013-08-25 12:49:24 +0800

    completed comway (not tested)

--- CS1020/src/Conway.java
+++ CS1020/src/Conway.java
@@ -24,14 +24,52 @@ public class Conway {
     // Compute the next step from board
     // - Save the result in board
     public void next() {
-        // Your code here...
+        // create a fresh new board for outputting the result
+        boolean[][] computedBoard = new boolean[length][length];
+
+        // check every cell in the board and evaluate accordingly based on scenarios
+        for(int row = 0; row < length; row++){
+            for(int column = 0; column < length; column++){
+                // since all 3 evaluations need to get the number of neighbours (living)
+                int living = countNeighbor(row, column);
+
+                // check whether is it alive or dead
+                if(board[row][column]){
+                    if(living < 2) computedBoard[row][column] = false; // if fewer than 2, dies
+                    else if(living <= 3) computedBoard[row][column] = true; // if with 2 or 3, lives
+                    else computedBoard[row][column] = false; // if more than 3, dies
+                }else{
+                    if(living == 3) computedBoard[row][column] = true; // if 3 exact, lives
+                    else computedBoard[row][column] = false; // else remains dead
+                }
+            }
+        }
+
+        // update the object variable to the new computed board
+        board = computedBoard;
     }
 
 
     // Count the number of living neighbors at index (i,j)
     // - Index starts from (0,0) to (length-1, length-1)
     public int countNeighbor(int i, int j) {
-        return 0;
+        // the number of living neighbours
+        int livingNeighbours = 0;
+
+        // check its neighbours at all direction
+        for(int row = i-1; row <= i+1; row++){
+            // check whether its before index 0 or exceed boundaries(ArrayOutOfBound)
+            if(row < 0 || row >= length) continue;
+            for(int column = j-1; column <= j+1; column++){
+                // check whether its before index 0 or exceed boundaries(ArrayOutOfBound)
+                if(column < 0 || column >= length) continue;
+                // check whether is it living
+                if(board[row][column]) livingNeighbours++;
+            }
+        }
+
+        // return the number of living neighbours excluding itself
+        return --livingNeighbours;
     }
 
     // Simulation
