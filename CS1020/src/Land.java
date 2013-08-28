5cd1e245751e4c0c49afa90111b0cb61acae4b96
Yeap Hooi Tong <hooitongw@gmail.com>
2013-08-28 13:15:39 +0800

    initial implementation of transformation

--- CS1020/src/Land.java
+++ CS1020/src/Land.java
@@ -1,5 +1,5 @@
 /**
- * This class accepts 3 types of input and produce the appropriate output based on given operation.
+ * This class a
  * @author Yeap Hooi Tong
  * @matric A0111736M
  * @date 21/8/13
@@ -8,11 +8,54 @@
 
 import java.util.*;
 
+public class Land {
+
+    public static void main(String[] args) {
+
+        // declare the necessary variables
+        Scanner sc = new Scanner(System.in);
+
+        // read user input
+        readInput(sc);
+
+    }
+
+    /**
+     * This method reads the grid size and number of the tree(s) (N)
+     * Afterwards, it reads N number of lines which are the coordinates of the tree
+     *
+     * @param sc The scanner object
+     */
+    private static void readInput(Scanner sc) {
+        // get the 2 necessary input from the user
+        int size = sc.nextInt();
+        int noOfTrees = sc.nextInt();
+
+        // create grid object (size x size) based on the user's input
+        Grid grid = new Grid(size, noOfTrees);
+
+        // set all default values to 1 (where 1 means it is a empty grid)
+        Arrays.fill(grid.matrix, 1);
+
+        // user inputs noOfTrees lines of coordinate, set the tree in the matrix
+        for (int i = 0; i < noOfTrees; i++) {
+            grid.matrix[sc.nextInt()][sc.nextInt()] = 0;
+        }
+    }
+}
+
 class Grid {
 
-    // declare the member field
+    int size; // width or length of the grid (W=L)
+    int noOfTrees; // number of trees in the grid (save the need to loop through)
+    int[][] matrix; // the 2d array that contains the grid. each integer represents the max empty space it can go
 
-    // declare the constructor
+    // overload constructor to create the 2d grid
+    public Grid(int size, int noOfTrees) {
+        matrix = new int[size][size];
+        this.size = size;
+        this.noOfTrees = noOfTrees;
+    }
 
     /**
      * checkNoTree   : to check whether the (size x size) square with upper-left coordinate
@@ -46,18 +89,4 @@ class Grid {
         return 0;
     }
 
-}
-
-public class Land {
-
-    public static void main(String[] args) {
-
-        // declare the necessary variables
-
-        // create new object from class Result
-
-        // declare a Scanner object to read input
-
-        // read input and process them accordingly
-    }
 }
