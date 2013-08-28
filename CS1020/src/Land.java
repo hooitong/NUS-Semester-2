cf873b0a691e72e2a2359062ca0aa7de46d9e85d
Yeap Hooi Tong <hooitongx@gmail.com>
2013-08-28 14:04:30 +0800

    Final implementation of Land class

--- CS1020/src/Land.java
+++ CS1020/src/Land.java
@@ -16,17 +16,22 @@ public class Land {
         Scanner sc = new Scanner(System.in);
 
         // read user input
-        readInput(sc);
+        Grid grid = readInput(sc);
 
+        // solve the equation
+        grid.calculate();
     }
 
     /**
      * This method reads the grid size and number of the tree(s) (N)
      * Afterwards, it reads N number of lines which are the coordinates of the tree
      *
-     * @param sc The scanner object
+     * @param sc
+     * The scanner object
+     * @return Grid
+     * The filled grid object
      */
-    private static void readInput(Scanner sc) {
+    private static Grid readInput(Scanner sc) {
         // get the 2 necessary input from the user
         int size = sc.nextInt();
         int noOfTrees = sc.nextInt();
@@ -35,12 +40,15 @@ public class Land {
         Grid grid = new Grid(size, noOfTrees);
 
         // set all default values to 1 (where 1 means it is a empty grid)
-        Arrays.fill(grid.matrix, 1);
+        for(int[] row : grid.matrix)
+            Arrays.fill(row, 1);
 
         // user inputs noOfTrees lines of coordinate, set the tree in the matrix
         for (int i = 0; i < noOfTrees; i++) {
-            grid.matrix[sc.nextInt()][sc.nextInt()] = 0;
+            grid.matrix[sc.nextInt()-1][sc.nextInt()-1] = 0;
         }
+
+        return grid;
     }
 }
 
@@ -58,35 +66,22 @@ class Grid {
     }
 
     /**
-     * checkNoTree   : to check whether the (size x size) square with upper-left coordinate
-     * (x, y) contains a tree
-     * Pre-condition :
-     * Post-condition:
+     * This method finds the the largest possible square with no trees in it [n(size*size)]
      */
-    public boolean checkNoTree(int x, int y, int size) {
-        // implementation
-        return false;
-    }
+    public void calculate(){
+        int largestSize = 0; // counter to keep track on the largest possible size
 
-    /**
-     * checkValidSize: to check whether it is possible to find a (size x size) square that contains
-     * no tree
-     * Pre-condition :
-     * Post-condition:
-     */
-    public boolean checkValidSize(int size) {
-        return false;
-    }
+        // iterate from the bottom right up (ignoring bottom and right edges)
+        for(int row = matrix.length-2; row >= 0; row--){
+            for(int column =  matrix[row].length-2; column >= 0; column--){
+                if(matrix[row][column] == 0) continue; // if its a tree, ignore
+                matrix[row][column] = 1 + Math.min(matrix[row][column+1], Math.min(matrix[row+1][column], matrix[row+1][column+1]));
+                if(matrix[row][column] > largestSize) largestSize = matrix[row][column]; // keep track of the largest square
+            }
+        }
 
-
-    /**
-     * solve         : use this method to find the largest size of a square with no trees
-     * Pre-condition :
-     * Post-condition:
-     */
-    public int solve() {
-        // implementation
-        return 0;
+        // print out the output
+        System.out.println(largestSize);
     }
 
 }
